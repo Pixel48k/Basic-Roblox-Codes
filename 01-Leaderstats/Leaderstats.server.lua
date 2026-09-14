c308d5de5b096637842252af7f3516586c576f36
Pixel48k <110926469+Pixel48k@users.noreply.github.com>
2026-09-14 20:13:34 +0530

    Add basic leaderstats example

---
+++ 01-Leaderstats/Leaderstats.server.lua
@@ -0,0 +1,14 @@
+-- Place this Script in ServerScriptService
+
+local Players = game:GetService("Players")
+
+Players.PlayerAdded:Connect(function(player)
+    local leaderstats = Instance.new("Folder")
+    leaderstats.Name = "leaderstats"
+    leaderstats.Parent = player
+
+    local money = Instance.new("IntValue")
+    money.Name = "Money"
+    money.Value = 0
+    money.Parent = leaderstats
+end)
